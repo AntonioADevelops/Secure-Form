e7e61485e35bd248f31d1ae5a6c013f3d5f78407
AntonioADevelops <ayalarantonio16@gmail.com>
2022-04-21 10:21:17 -0700

    changes

--- webapp.py
+++ webapp.py
@@ -1,10 +1,22 @@
-from flask import Flask, request_started, url_for, render_template, request, Markup
+import os
+from flask import Flask, url_for, render_template, request
+from flask import redirect
+from flask import session
 
 app = Flask(__name__)
 
 @app.route("/")
 def render_home():
     return render_template("home.html")
+
+@app.route("/userinfo")
+def render_userquiz():
+    return render_template("userinfo.html")
+
+@app.route("/startover")
+def render_restart():
+    # session.clear()
+    return redirect("/")
 
 if __name__=="__main__":
     app.run(debug=False)
