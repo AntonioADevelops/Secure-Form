6da8a08a2f137a61222b30176baa804811d23f73
AntonioADevelops <ayalarantonio16@gmail.com>
2022-04-26 09:57:59 -0700

    changes

--- webapp.py
+++ webapp.py
@@ -2,9 +2,10 @@ from distutils.log import error
 from flask import Flask, url_for, render_template, request
 from flask import redirect
 from flask import session
-from flask import flash
+import os
 
 app = Flask(__name__)
+app.secret_key=os.environ["SECRET_KEY"];
 
 @app.route("/")
 def render_home():
@@ -12,39 +13,27 @@ def render_home():
 
 @app.route("/geography", methods=['GET', 'POST'])
 def render_geography():
-    if request.method == 'POST':
+    if request.method == "POST":
         return render_template("geography.html")
     else:
-        flash("Refreshing the page leads to an incomplete result, please press the restard button to restart the quiz")
+        session.clear()
         return redirect("/")
 
 @app.route("/math", methods=['GET', 'POST'])
 def render_math():
-    if request.method == 'POST':
-        return render_template("math.html")
-    else:
-        flash("Refreshing the page leads to an incomplete result, please press the restard button to restart the quiz")
-        return redirect("/")
-    
+    return render_template("math.html")
+
 @app.route("/history", methods=['GET', 'POST'])
 def render_history():
-    if request.method == 'POST':
-        return render_template("history.html")
-    else:
-        flash("Refreshing the page leads to an incomplete result, please press the restard button to restart the quiz")
-        return redirect("/")
+    return render_template("history.html")
 
 @app.route("/results", methods=['GET', 'POST'])
 def render_results():
-    if request.method == 'POST':
-        return render_template("results.html")
-    else:
-        flash("Refreshing the page leads to an incomplete result, please press the restard button to restart the quiz")
-        return redirect("/")
+    return render_template("results.html")
 
 @app.route("/startover", methods=['GET', 'POST'])
 def render_restart():
-    # session.clear()
+    session.clear()
     return redirect("/")
 
 if __name__=="__main__":
