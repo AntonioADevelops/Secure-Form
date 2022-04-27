644c3a9ffddc18235045b22c7f046b1a98850451
AntonioADevelops <ayalarantonio16@gmail.com>
2022-04-27 09:58:06 -0700

    changes

--- webapp.py
+++ webapp.py
@@ -29,6 +29,32 @@ def render_history():
 
 @app.route("/results", methods=['GET', 'POST'])
 def render_results():
+    g_correct = 'Australia'
+    g_correct = 'Mt. Everest'
+    g_correct = 'China'
+    g_correct = 'Washington D.C.'
+    m_correct = '2'
+    m_correct = 'ax^2 + bx + c'
+    m_correct = '67'
+    m_correct = '64'
+    h_correct = '7th Century BC'
+    h_correct = 'Australia'
+    h_correct = 'Australia'
+    h_correct = 'Australia'
+    g_answ1 = request.forms['smallercont']
+    g_answ2 = request.forms['tallestmt']
+    g_answ3 = request.forms['greatestpop']
+    g_answ4 = request.forms['capitalstate']
+    m_answ1 = request.forms['easy']
+    m_answ2 = request.forms['quad']
+    m_answ3 = request.forms['prime']
+    m_answ4 = request.forms['sqandcb']
+    h_answ1 = request.forms['wallchina']
+    h_answ2 = request.forms['xmastruce']
+    h_answ3 = request.forms['plaguestart']
+    h_answ4 = request.forms['romanfall']
+    
+    
     return render_template("results.html")
 
 @app.route("/startover", methods=['GET', 'POST'])
